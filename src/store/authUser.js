b3d90dd18fd4f3edf8496ad4ba7823bfd46c02f6
Utkarsh1228 <110552854+Utkarsh1228@users.noreply.github.com>
2024-10-05 17:00:54 +0530

    fixed

--- src/store/authUser.js
+++ src/store/authUser.js
@@ -4,7 +4,6 @@ import { create } from "zustand";
 
 const apiUrl = import.meta.env.VITE_BACKEND_URL;
 
-
 export const useAuthStore = create((set) => ({
 	user: null,
 	isSigningUp: false,
@@ -14,7 +13,7 @@ export const useAuthStore = create((set) => ({
 	signup: async (credentials) => {
 		set({ isSigningUp: true });
 		try {
-			const response = await axios.post(apiUrl + "/api/v1/auth/signup", credentials);
+			const response = await axios.post(apiUrl + "/api/v1/auth/signup", credentials, { withCredentials: true });
 			set({ user: response.data.user, isSigningUp: false });
 			toast.success("Account created successfully");
 		} catch (error) {
@@ -25,7 +24,7 @@ export const useAuthStore = create((set) => ({
 	login: async (credentials) => {
 		set({ isLoggingIn: true });
 		try {
-			const response = await axios.post(apiUrl + "/api/v1/auth/login", credentials);
+			const response = await axios.post(apiUrl + "/api/v1/auth/login", credentials, { withCredentials: true });
 			set({ user: response.data.user, isLoggingIn: false });
 		} catch (error) {
 			set({ isLoggingIn: false, user: null });
@@ -46,7 +45,7 @@ export const useAuthStore = create((set) => ({
 	authCheck: async () => {
 		set({ isCheckingAuth: true });
 		try {
-			const response = await axios.get(apiUrl + "/api/v1/auth/authCheck");
+			const response = await axios.get(apiUrl + "/api/v1/auth/authCheck", { withCredentials: true });
 
 			set({ user: response.data.user, isCheckingAuth: false });
 		} catch (error) {
